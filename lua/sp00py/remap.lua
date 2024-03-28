dc12d57b72df7ec3c0398cd584af559f518ae435
Ethan Lekki <39201831+Awes0meEman@users.noreply.github.com>
2024-03-27 23:19:19 -0400

    Added some remaps

--- lua/sp00py/remap.lua
+++ lua/sp00py/remap.lua
@@ -5,3 +5,9 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 
 vim.keymap.set("x", "<leader>p", "\"_dP")
+
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+vim.keymap.set("n", "Q", "<nop>")
