ac6f37cf4f8738aded89c1e967d66e364c1d95dc
elekki <162749492+elekki@users.noreply.github.com>
2024-08-13 15:19:10 -0400

    cleaned up remaps

--- lua/sp00py/lazy/lsp.lua
+++ lua/sp00py/lazy/lsp.lua
@@ -78,9 +78,10 @@ return {
                 end,
             },
             mapping = cmp.mapping.preset.insert({
-                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
+                ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
+                ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+                ['<Enter>'] = cmp.mapping.confirm({ select = true }),
                 ["<C-Space>"] = cmp.mapping.complete(),
             }),
             sources = cmp.config.sources({
