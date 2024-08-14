be564cce18afad0394e70404b44fddadff701c55
elekki <162749492+elekki@users.noreply.github.com>
2024-08-14 09:16:40 -0400

    added check for ncat to stop errors on systems that don't have ncat installed

--- lua/sp00py/init.lua
+++ lua/sp00py/init.lua
@@ -9,30 +9,30 @@ local MainGroup = augroup('MainGroup', {})
 local YankGroup = augroup('HighlightYank', {})
 
 function R(name)
-	require("plenary.reload").reload_module(name)
+    require("plenary.reload").reload_module(name)
 end
 
 vim.filetype.add({
-	extension = {
-		temp1 = 'temp1',
-	}
+    extension = {
+        temp1 = 'temp1',
+    }
 })
 
 autocmd('TextYankPost', {
-	group = YankGroup,
-	pattern = "*",
-	callback = function()
-		vim.highlight.on_yank({
-			higroup = 'IncSearch',
-			timeout = 40,
-		})
-	end,
+    group = YankGroup,
+    pattern = "*",
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
 })
 
-autocmd({"BufWritePre"}, {
-	group = MainGroup,
-	pattern = "*",
-	command = [[%s/\s\+$//e]],
+autocmd({ "BufWritePre" }, {
+    group = MainGroup,
+    pattern = "*",
+    command = [[%s/\s\+$//e]],
 })
 
 autocmd('LspAttach', {
@@ -53,15 +53,18 @@ autocmd('LspAttach', {
 })
 
 -- setup for Godot LSP
-local port = os.getenv('GDScript_Port') or '6005'
-local cmd = {'ncat', '127.0.0.1', port}
-local pipe = [[\\.\pipe\godot.pipe]]
+if os.getenv('ncat') ~= nil then
+    local port = os.getenv('GDScript_Port') or '6005'
 
-vim.lsp.start({
-  name = 'Godot',
-  cmd = cmd,
-  root_dir = vim.fs.dirname(vim.fs.find({ 'project.godot', '.git' }, { upward = true })[1]),
-  on_attach = function(client, bufnr)
-    vim.api.nvim_command([[echo serverstart(']] .. pipe .. [[')]])
-  end
-})
+    local cmd = { 'ncat', '127.0.0.1', port }
+    local pipe = [[\\.\pipe\godot.pipe]]
+
+    vim.lsp.start({
+        name = 'Godot',
+        cmd = cmd,
+        root_dir = vim.fs.dirname(vim.fs.find({ 'project.godot', '.git' }, { upward = true })[1]),
+        on_attach = function(client, bufnr)
+            vim.api.nvim_command([[echo serverstart(']] .. pipe .. [[')]])
+        end
+    })
+end
