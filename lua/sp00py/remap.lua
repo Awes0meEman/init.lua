497161923ddd692078b39824f8250ef1093be924
elekki <162749492+elekki@users.noreply.github.com>
2024-08-14 13:03:51 -0400

    add remaps

--- lua/sp00py/remap.lua
+++ lua/sp00py/remap.lua
@@ -16,6 +16,8 @@ vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
+vim.keymap.set("i", "jj", "<Esc>")
+vim.keymap.set("i", "kk", "<Esc>")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
